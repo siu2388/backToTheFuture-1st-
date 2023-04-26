d5c0459632c988622e167d4a63915da95d5b566f
siu2388 <swear428@naver.com>
2023-04-26 13:50:17 +0900

    feat.전체파일만 생성

---
+++ back/src/db/schemas/award.js
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+
+const AwardSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    grade: {
+      type: String,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+      default: "설명이 아직 없습니다. 추가해 주세요.",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const AwardModel = model("Award", AwardSchema);
+
+export { AwardModel };
