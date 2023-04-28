28b81f9074282117a0ee46f1daeffc54ecbac552
yyoungl <127117707+yyoungl@users.noreply.github.com>
2023-04-28 16:32:45 +0900

    feat.정말로삭제하시겠습니까

--- front/src/components/award/AwardCard.js
+++ front/src/components/award/AwardCard.js
@@ -1,15 +1,19 @@
-import { Card, Button, Row, Col } from "react-bootstrap";
-import * as Api from "../../api"
-
+import { Card, Modal, Button, Row, Col } from "react-bootstrap";
+import {useState} from 'react';
+import * as Api from "../../api";
 
 function AwardCard({ award, isEditable, setIsEditing, setAwards }) {
   const handleDelete = async () => {
     await Api.delete("awards", award.id).then(() => {
-      setAwards((prevAwards) => 
-      prevAwards.filter((award) => award.id !== award.id)
+      setAwards((prevAwards) =>
+        prevAwards.filter((award) => award.id !== award.id)
       );
     });
-  }
+  };
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <Card.Text>
@@ -33,17 +37,32 @@ function AwardCard({ award, isEditable, setIsEditing, setAwards }) {
             >
               편집
             </Button>
-            <Button
-              variant="outline-danger"
-              size="sm"
-              // 함수 기능 넣기
-              onClick = {() => {
-                handleDelete();
-                alert("삭제합니다");
-              }}
-            >
-              삭제
-            </Button>
+            <>
+              <Button variant="outline-danger" onClick={handleShow} size = "sm" >
+                삭제
+              </Button>
+
+              <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                  <Modal.Title>삭제</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>정말로 삭제하시겠습니까? T.T</Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    취소
+                  </Button>
+                  <Button
+                    variant="primary"
+                    onClick={() => {
+                      handleClose();
+                      handleDelete();
+                    }}
+                  >
+                    변경 내용 저장
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </>
           </Col>
         )}
       </Row>
