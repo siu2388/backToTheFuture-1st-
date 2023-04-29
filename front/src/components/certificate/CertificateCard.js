977f9f2d72f56d7729876af9f1173a5b6ac126da
siu2388 <swear428@naver.com>
2023-04-30 00:35:43 +0900

    certificate삭제에러

--- front/src/components/certificate/CertificateCard.js
+++ front/src/components/certificate/CertificateCard.js
@@ -1,6 +1,30 @@
-import { Card, Button, Row, Col } from "react-bootstrap";
+import { Card, Modal, Button, Row, Col } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import * as Api from "../../api";
 
-function CertificateCard({ certificate, isEditable, setIsEditing }) {
+function CertificateCard({
+  certificate,
+  isEditable,
+  setIsEditing,
+  setCertificates,
+}) {
+  const handleDelete = async () => {
+    await Api.delete("certificates", certificate.id).then(() => {
+      setCertificates((prevCertificates) =>
+        prevCertificates.filter(
+          (prevCertificate) => prevCertificate.id !== certificate.id
+        )
+      );
+    });
+  };
+
+  useEffect(() => {}, [certificate]);
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <Card.Text>
       <Row className="align-items-center">
@@ -8,25 +32,48 @@ function CertificateCard({ certificate, isEditable, setIsEditing }) {
           <span>{certificate.title}</span>
           <br />
           <span className="text-muted">{certificate.authority}</span>
-          <br />          
+          <br />
           <span className="text-muted">{certificate.registerNum}</span>
           <br />
-          <span className="text-muted">{certificate.grade}</span>          
+          <span className="text-muted">{certificate.grade}</span>
         </Col>
 
         {isEditable && (
-          <Col xs lg="3" style={{ display: 'flex', alignItems: 'center' }}>
+          <Col xs lg="1">
             <Button
               variant="outline-info"
               size="sm"
-              onClick={() => setIsEditing((prev)=>!prev)}
+              onClick={() => setIsEditing((prev) => !prev)}
               className="me-1"
-            >편집</Button>
-            <Button 
-            variant="outline-danger"
-            size="sm"
-            // 함수 기능 넣기 
-            >삭제</Button>
+            >
+              편집
+            </Button>
+            <>
+              <Button variant="outline-danger" onClick={handleShow} size="sm">
+                삭제
+              </Button>
+
+              <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                  <Modal.Title>삭제</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>정말로 삭제하시겠습니까? T.T</Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    취소
+                  </Button>
+                  <Button
+                    variant="primary"
+                    onClick={() => {
+                      handleClose();
+                      handleDelete();
+                    }}
+                  >
+                    확인
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </>
           </Col>
         )}
       </Row>
