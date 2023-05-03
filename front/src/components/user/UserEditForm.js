0834546d778a1729f1e9661d9ee725165a26894e
yyoungl <127117707+yyoungl@users.noreply.github.com>
2023-05-03 14:09:04 +0900

    fix.UserEditForm수정

--- front/src/components/user/UserEditForm.js
+++ front/src/components/user/UserEditForm.js
@@ -83,7 +83,7 @@ function UserEditForm({ user, setIsEditing, setUser }) {
   };
 
   return (
-    <Modal show={show} animation={false} onHide={handleClose}>
+    <Modal show={show} animation={false}>
       <Modal.Header>
         <Modal.Title>유저 정보 수정</Modal.Title>
       </Modal.Header>
