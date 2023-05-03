f684957059192ddcbcacbde9933be718a0e6c075
yyoungl <127117707+yyoungl@users.noreply.github.com>
2023-05-03 21:16:04 +0900

    feat.guestaddform 조금..

--- front/src/components/guestbook/GuestBookAddForm.js
+++ front/src/components/guestbook/GuestBookAddForm.js
@@ -1,33 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Button, Form, Col, Row } from "react-bootstrap";
 import * as Api from "../../api";
+import { UserStateContext } from "../../App";
 
-function GuestBookAddForm({ guestBookPageOwnerId, setGuestBooks, setIsAdding }) {
+function GuestBookAddForm({ guestBookPageOwner, setGuestBooks, setIsAdding }) {
   //useState로 title 상태를 생성함.
-  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const userState = useContext(UserStateContext);
   //useState로 description 상태를 생성함.
-  const [description, setDescription] = useState("");
-  const [grade, setGrade] = useState("");
-  const [date, setDate] = useState("");
+  const [authorId, setAuthorId] = useState(userState.user?.id);
 
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     e.stopPropagation();
 
     // guestBookPageOwnerId를 user_id 변수에 할당함.
-    const user_id = guestBookPageOwnerId;
+    const receiverId = guestBookPageOwner.id;
+ 
 
     // "guestBook/create" 엔드포인트로 post요청함.
     await Api.post("guestBook/create", {
-      user_id: guestBookPageOwnerId,
-      title,
-      grade,
-      date,
-      description,
+      receiverId: guestBookPageOwner.id,
+      authorId: userState.user.id,
+      content,
     });
 
     // "guestBooklist/유저id" 엔드포인트로 get요청함.
-    const res = await Api.get("guestBooklist", user_id);
+    const res = await Api.get("guestBooklist", receiverId);
     // guestBooks를 response의 data로 세팅함.
     setGuestBooks(res.data);
     // guestBook를 추가하는 과정이 끝났으므로, isAdding을 false로 세팅함.
@@ -37,44 +37,17 @@ function GuestBookAddForm({ guestBookPageOwnerId, setGuestBooks, setIsAdding }) 
   return (
     <Form onSubmit={handleSubmit}>
       
-      <label htmlFor="floatingInputCustom">수상내역</label>
+      <label htmlFor="floatingInputCustom">발자국 남기기</label>
         <Form.Control
           id="floatingInputCustom"
           type="text"
-          value={title}
-          placeholder="수상내역"
-          onChange={(e) => setTitle(e.target.value)}
+          value={content}
+          placeholder="하고 싶은 말을 남겨 보세요."
+          onChange={(e) => setContent(e.target.value)}
         />
 
 
       
-      <label htmlFor="floatingInputCustom">상</label>
-        <Form.Control
-          id="floatingInputCustom"
-          type="text"
-          placeholder="예: 금상"
-          value={grade}
-          onChange={(e) => setGrade(e.target.value)}
-        />
-
-
-      <label htmlFor="floatingInputCustom">수상년월</label>       
-        <Form.Control
-          id="floatingInputCustom"
-          type="text"
-          placeholder="예 : 2020-02"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-
-
-      <label htmlFor="floatingInputCustom">상세내역</label>    
-        <Form.Control
-          type="text"
-          placeholder="상세내역"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
 
       <Form.Group as={Row} className="mt-3 text-center">
         <Col sm={{ span: 20 }}>
