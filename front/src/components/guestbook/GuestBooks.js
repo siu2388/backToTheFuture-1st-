55620f7ec982bd35c662f4770f370ecb335cff38
yyoungl <127117707+yyoungl@users.noreply.github.com>
2023-05-04 16:09:52 +0900

    fix.방명록글쓰기창 위치변경

--- front/src/components/guestbook/GuestBooks.js
+++ front/src/components/guestbook/GuestBooks.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useContext, useState } from "react";
 import { Card, Button, Row, Col } from "react-bootstrap";
-import * as Api from '../../api';
+import * as Api from "../../api";
 import GuestBook from "./GuestBook";
 import GuestBookAddForm from "./GuestBookAddForm";
 
 import { UserStateContext } from "../../App";
-
 
 function GuestBooks({ guestBookPageOwnerId }) {
   //useState로 guestBooks 상태를 생성함.
@@ -14,44 +13,50 @@ function GuestBooks({ guestBookPageOwnerId }) {
   const [isAdding, setIsAdding] = useState(false);
   const userState = useContext(UserStateContext);
 
-
-
   useEffect(() => {
     // "guestBooklist/유저id"로 GET 요청하고, response의 data로 guestBooks를 세팅함.
-    Api.get("guestBooklist", guestBookPageOwnerId).then((res) => setGuestBooks(res.data));
+    Api.get("guestBooklist", guestBookPageOwnerId).then((res) =>
+      setGuestBooks(res.data)
+    );
   }, [guestBookPageOwnerId]);
-
 
   return (
     <Card>
       <Card.Body>
-        <Card.Title style = {{display: "inline-block", marginRight: "10px"}} >방명록</Card.Title>
-        { userState.user &&  (
-          <button className = "btn-post" style = {{ display: "inline-block" }} onClick={() => setIsAdding(true)}>글쓰기</button>
-        ) }
-        
-        { guestBooks
-        .filter((guestBook) => guestBook.receiverId === guestBookPageOwnerId)
-        .map((guestBook) => (
-          <GuestBook
-            key={guestBook.id}
-            guestBook={guestBook}
-            setGuestBooks={setGuestBooks}
-            isEditable={guestBook.receiverId === userState.user?.id
-            ||guestBook.authorId=== userState.user?.id
-          }
-
-          />
-        ))}
+        <Card.Title style={{ display: "inline-block", marginRight: "10px" }}>
+          방명록
+        </Card.Title>
+        {userState.user && (
+          <button
+            className="btn-post"
+            style={{ display: "inline-block" }}
+            onClick={() => setIsAdding(true)}
+          >
+            글쓰기
+          </button>
+        )}
 
         {isAdding && (
           <GuestBookAddForm
             guestBookPageOwnerId={guestBookPageOwnerId}
             setGuestBooks={setGuestBooks}
             setIsAdding={setIsAdding}
-            
           />
         )}
+
+        {guestBooks
+          .filter((guestBook) => guestBook.receiverId === guestBookPageOwnerId)
+          .map((guestBook) => (
+            <GuestBook
+              key={guestBook.id}
+              guestBook={guestBook}
+              setGuestBooks={setGuestBooks}
+              isEditable={
+                guestBook.receiverId === userState.user?.id ||
+                guestBook.authorId === userState.user?.id
+              }
+            />
+          ))}
       </Card.Body>
     </Card>
   );
