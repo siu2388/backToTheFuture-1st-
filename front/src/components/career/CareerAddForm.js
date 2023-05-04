4a63913175fbd6c1d368df8fc1a85546d3efde28
yyoungl <127117707+yyoungl@users.noreply.github.com>
2023-05-04 19:27:50 +0900

    feat.datepicker range 삽입

--- front/src/components/career/CareerAddForm.js
+++ front/src/components/career/CareerAddForm.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { Form, Col, Row } from "react-bootstrap";
-import { Form, Col, Row } from "react-bootstrap";
+
 import * as Api from "../../api";
 import DatePicker from "react-datepicker";
-import DatePicker from "react-datepicker";
+
 
 function CareerAddForm({ portfolioOwnerId, setCareers, setIsAdding }) {
   //useState로 title 상태를 생성함.
