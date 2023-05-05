5e0e0cd9d583de258e690ade3591e0482cb2b8c0
[AI7/2반]이지윤 <little8867@gmail.com>
2023-05-05 22:05:16 +0900

    fix:모바일레이아웃수정

--- front/src/components/user/UserCard.js
+++ front/src/components/user/UserCard.js
@@ -25,7 +25,7 @@ function UserCard({ user, setIsEditing, isEditable, isNetwork }) {
         <Card.Text>{user?.description}</Card.Text>
         <a href={user?.github} rel="noreferrer" target="_blank">
           <img
-            src="images/github.png"
+            src="/images/github.png"
             alt="Github"
             style={{ width: "1.7rem", marginRight: "10px" }}
           />
@@ -33,7 +33,7 @@ function UserCard({ user, setIsEditing, isEditable, isNetwork }) {
         <a href={user?.blog} rel="noreferrer" target="_blank">
           <img
             style={{ width: "1.7rem"}}
-            src="images/tistory.png"
+            src="/images/tistory.png"
             alt="Blog"
           />
         </a>
